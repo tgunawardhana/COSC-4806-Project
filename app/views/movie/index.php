8d53d0da724f44d4832024c2e5b28fe183ae760f
tgunawardhana <tgunawardhana@algomau.ca>
2025-07-25 04:48:10 +0000

    Add star rating code and updated the search fields to display in one line

--- app/views/movie/index.php
+++ app/views/movie/index.php
@@ -11,36 +11,70 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-
-            <form method="post" action="/movie/search">
-                <div class="form-group">
-                    <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Enter title">
-                </div>
-                    <br>
-                <div class="form-group">
-                    <label for="year">Year</label>
-                    <input type="text" class="form-control" id="year" name="year" placeholder="Enter year">
+           
+        <form method="post" action="/movie/search">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" placeholder="Enter title">
                     </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="year">Year</label>
+                        <input type="text" class="form-control" id="year" name="year" placeholder="Enter year">
+                    </div>
+                </div>
+                <div class="col">
                     <br>
                     <button type="submit" class="btn btn-primary">Search</button>
-                    <br>
-           </form>
+                </div>
+            </div>
+       </form>
 
-        </div>
+       
     </div>
     <br>
     <br>
-    <div class="col-lg-12">
-            <?php foreach ($data['results'] as $result): ?>
-                <?php echo $result['Title'] ?>
+       
+        <?php foreach ($data['results'] as $result): ?>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+                <img src="<?php echo $result['Poster'] ?>" alt="Poster">
+            </div>
+            <div class="col">
+                <h1> <?php echo $result['Title'] ?></h1>
+               
                 <br>
                 <?php echo $result['Year'] ?>
                 <br>
+
+
+
                 
-            
+                    <div >
+                        <h5 class="mb-4">Rate this movie</h5>
+                        <div class="star-rating animated-stars">
+                            <input type="radio" id="star5" name="rating" value="5">
+                            <label for="star5" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star4" name="rating" value="4">
+                            <label for="star4" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star3" name="rating" value="3">
+                            <label for="star3" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star2" name="rating" value="2">
+                            <label for="star2" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star1" name="rating" value="1">
+                            <label for="star1" class="bi bi-star-fill"></label>
+                        </div>
+                        <p class="text-muted mt-2">Click to rate</p>
+                    </div>
+               
+                
+            </div>
+          </div>
+        </div>
             <br>    
   
                                                  
